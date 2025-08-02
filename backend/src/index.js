f1894dce1f1eb563d3b0594b5246a7996f735098
Samrat Subedi <samratsubedi9@gmail.com>
2025-08-02 22:32:07 +0545

    UI logics implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
@@ -10,6 +11,13 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
